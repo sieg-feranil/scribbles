4d8687321bc029be273431f2598c1712d1087f7f
sieg-feranil <sieg.ferrer.feranil@gmail.com>
2023-03-28 16:10:45 +0200

    2803

---
+++ js pom/2803/index.js
@@ -0,0 +1,37 @@
+
+let a = 'ciao'
+let b = 'caao'
+let max
+if (a.length > b.length) {
+    max = a;
+}
+else if (a.length == b.length) {
+    max = a+' '+b
+}
+else {
+    max = b;
+}
+if (a.length == b.length) {
+    console.log('le due stringhe hanno lunghezza uguale e sono: ' + max);
+}
+else {
+    
+    console.log('la stringa più lunga è:' + max);
+}
+
+if (a != b) {
+    console.log('sono diversi');
+}
+else {
+    console.log('sono uguali');
+}
+let f = 'mela'
+console.log(f.charAt(3)+'-'+f.charAt(2)+'-'+f.charAt(1)+'-'+f.charAt(0));
+console.log(f.length);
+
+
+
+// for (let i = f.length; i >= 0; i--) {
+//     console.log(f.charAt(i));
+    
+// }
